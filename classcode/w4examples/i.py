84943dc68cad6e74fb2850d6d907f7edec7e208d
Acacia Ackles <alackles@protonmail.com>
2024-01-26 08:11:44 -0600

    Add week 4

---
+++ classcode/w4examples/i.py
@@ -0,0 +1,4 @@
+list_of_nums = [1, 4, 5, 9, 10]
+for i in range(len(list_of_nums)):
+    print(i, list_of_nums[i])
+print(i)
---
+++ classcode/w4examples/loops.py
@@ -0,0 +1,28 @@
+i = 0 
+while i < 13:
+    print("Bloody Mary!", i)
+    i = i + 1
+    print("Now i is", i)
+
+print("---")
+
+for i in range(0, 13):
+    print("Bloody Mary", i)
+
+mystring = "Hello everybody"
+for index in range(len(mystring)):
+    print(index)
+    print(mystring[index])
+
+for letter in mystring:
+    print(letter)
+
+
+list_of_nums = [1, 4, 5, 8, 9]
+print("List:")
+for i in list_of_nums:
+    print(i)
+
+print("Indexed:")
+for i in range(len(list_of_nums)):
+    print(i, list_of_nums[i])
